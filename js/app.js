5493a9168ed5d846157db24efcd78cf4dfec9648
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-05-22 21:32:47 -1000

    add ex 4

--- js/app.js
+++ js/app.js
@@ -37,6 +37,12 @@ function zoom(){
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ["Chicken McNuggets", "Shamrock Shake", "Large Fries"];
+
+function valueMenu(){
+	var menu = document.getElementById("menu");
+	menu.innerHTML = menuItems;
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
